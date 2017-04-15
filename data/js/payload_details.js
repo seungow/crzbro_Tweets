ea21076b3ee362d8ebed51f1ccd329d26416cebd
seungow <seungow@gmail.com>
2017-04-15 12:12:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 126796,
-	"created_at": "Fri Apr 14 19:12:05 +0000 2017",
+	"tweets": 126797,
+	"created_at": "Sat Apr 15 03:12:01 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,337 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Sat Apr 15 02:51:21 +0000 2017",
+		"id": 853078261231792100,
+		"id_str": "853078261231792128",
+		"text": "RT @mf_dice: 아무리 뛰어봤자 심슨안에 있다 https://t.co/jFebk3Nk5d",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mf_dice",
+					"name": "[머글당]MF Dice K.one",
+					"id": 109189449,
+					"id_str": "109189449",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 852689716612694000,
+					"id_str": "852689716612694016",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+					"url": "https://t.co/jFebk3Nk5d",
+					"display_url": "pic.twitter.com/jFebk3Nk5d",
+					"expanded_url": "https://twitter.com/mf_dice/status/852689734899908608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 460,
+							"h": 295,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 460,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 460,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 852689734899908600,
+					"source_status_id_str": "852689734899908608",
+					"source_user_id": 109189449,
+					"source_user_id_str": "109189449"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 852689716612694000,
+					"id_str": "852689716612694016",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+					"url": "https://t.co/jFebk3Nk5d",
+					"display_url": "pic.twitter.com/jFebk3Nk5d",
+					"expanded_url": "https://twitter.com/mf_dice/status/852689734899908608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 460,
+							"h": 295,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 460,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 460,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 852689734899908600,
+					"source_status_id_str": "852689734899908608",
+					"source_user_id": 109189449,
+					"source_user_id_str": "109189449"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95255804,
+			"id_str": "95255804",
+			"name": "미오",
+			"screen_name": "crzbro",
+			"location": "sol",
+			"description": "밥을 위해 법을 하는 불쌍한 법쟁이",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5711,
+			"friends_count": 1127,
+			"listed_count": 124,
+			"created_at": "Mon Dec 07 18:44:23 +0000 2009",
+			"favourites_count": 5488,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 125645,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/804406816092209152/4m9Qk-sm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/804406816092209152/4m9Qk-sm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95255804/1442844547",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Apr 14 01:07:29 +0000 2017",
+			"id": 852689734899908600,
+			"id_str": "852689734899908608",
+			"text": "아무리 뛰어봤자 심슨안에 있다 https://t.co/jFebk3Nk5d",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 852689716612694000,
+						"id_str": "852689716612694016",
+						"indices": [
+							17,
+							40
+						],
+						"media_url": "http://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+						"url": "https://t.co/jFebk3Nk5d",
+						"display_url": "pic.twitter.com/jFebk3Nk5d",
+						"expanded_url": "https://twitter.com/mf_dice/status/852689734899908608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 460,
+								"h": 295,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 460,
+								"h": 295,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 460,
+								"h": 295,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 852689716612694000,
+						"id_str": "852689716612694016",
+						"indices": [
+							17,
+							40
+						],
+						"media_url": "http://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C9Vcx45UAAA7lVf.jpg",
+						"url": "https://t.co/jFebk3Nk5d",
+						"display_url": "pic.twitter.com/jFebk3Nk5d",
+						"expanded_url": "https://twitter.com/mf_dice/status/852689734899908608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 460,
+								"h": 295,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 460,
+								"h": 295,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 460,
+								"h": 295,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 109189449,
+				"id_str": "109189449",
+				"name": "[머글당]MF Dice K.one",
+				"screen_name": "mf_dice",
+				"location": "사무실 1열",
+				"description": "누구도 원하지 않던 당신의 머글. 이젠 온 국민이 응원합니다.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3732,
+				"friends_count": 1450,
+				"listed_count": 87,
+				"created_at": "Thu Jan 28 07:06:33 +0000 2010",
+				"favourites_count": 1929,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 369397,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/137954408/IMGP7356.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/137954408/IMGP7356.jpg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/848189056420728832/6e72NMG__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/848189056420728832/6e72NMG__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/109189449/1478174013",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 313,
+			"favorite_count": 43,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 313,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Fri Apr 14 17:40:17 +0000 2017",
 		"id": 852939578671415300,
 		"id_str": "852939578671415296",
