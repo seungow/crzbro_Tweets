52c561ba29a114b9daeaa29a5f0fe46d4c0ab7c3
seungow <seungow@gmail.com>
2017-04-22 11:12:10 +0900

    Added 7 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_04.js",
 		"year": 2017,
 		"var_name": "tweets_2017_04",
-		"tweet_count": 868,
+		"tweet_count": 875,
 		"month": 4
 	},
 	{
