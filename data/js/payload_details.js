200f7652a4dda2c522b316c5267cbf897219e970
seungow <seungow@gmail.com>
2017-04-24 06:12:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 127304,
-	"created_at": "Sun Apr 23 19:12:11 +0000 2017",
+	"tweets": 127305,
+	"created_at": "Sun Apr 23 21:12:10 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_04.js",
 		"year": 2017,
 		"var_name": "tweets_2017_04",
-		"tweet_count": 964,
+		"tweet_count": 965,
 		"month": 4
 	},
 	{
